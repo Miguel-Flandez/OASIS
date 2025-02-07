5723d2970c6bf8ea3f0d8bc5e2c08d0745abad9e
Miguel Flandez <immfpnvs2@gmail.com>
2025-02-07 17:22:59 +0800

    Student Information

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,28 @@
 const homeButton = document.getElementById('home-button');
 const opcButton = document.getElementById('opc-button')
+const studentInformationButton = document.getElementById('student-info-button')
+const profileButton = document.getElementById('profile-button');
 
 homeButton.onclick = function(){
     window.location.href = 'studentHome.html';
 }
 opcButton.onclick = function(){
     window.location.href = 'online-payment-center.html';
+}
+studentInformationButton.onclick = function(){
+    window.location.href = 'studentInformation.html'
+}
+profileButton.onclick = function(){
+    window.location.href = 'userProfile.html'
+}
+
+const sidebarToggle = document.querySelector('.fa-solid.fa-bars');
+const sidebar = document.getElementById('sidebar');
+
+sidebarToggle.onclick = function(){
+    if(!sidebar.classList.contains('hide-sidebar')){
+        sidebar.classList.add('hide-sidebar');
+    }else{
+        sidebar.classList.remove('hide-sidebar');
+    }
 }
